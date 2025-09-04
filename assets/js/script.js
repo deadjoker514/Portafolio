094da0f6f45ffdd182d33e5f881f508981683e0a
Enrique <213696313+deadjoker514@users.noreply.github.com>
2025-09-04 08:40:44 -0600

    Correcciones de datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,15 +130,14 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
-
+    const categories = filterItems[i].dataset.category.split(',').map(cat => cat.trim().toLowerCase());
     if (selectedValue === "todos" || selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+    } else if (categories.includes(selectedValue)) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
 
 }
